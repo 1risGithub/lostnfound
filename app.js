58bb5cd43ae4439f8e7c9086a4fc9ef012dd268c
Iris <Iris.khanatip@gmail.com>
2025-03-08 05:56:14 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,20 +7,18 @@ const PORT = process.env.PORT || 4000;
 
 // Connect to database using Environment Variables from Railway
 const db = mysql.createPool({
-  uri:
-    process.env.MYSQL_URL ||
-    "mysql://root:cSKvpWezJyVavIilvFdEpQalLALHCxud@nozomi.proxy.rlwy.net:34300/railway",
+  uri: process.env.MYSQL_URL || "mysql://root:cSKvpWezJyVavIilvFdEpQalLALHCxud@nozomi.proxy.rlwy.net:34300/railway",
 });
 
 // Middleware support JSON
 app.use(express.json());
 
-// Serve static files from the 'lostnfound' directory
-app.use(express.static(path.join(__dirname, "lostnfound")));
+// Serve static files from the root directory
+app.use(express.static(__dirname));
 
 // Serve index.html at the root path "/"
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "lostnfound", "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 // Example API endpoint at "/API"
