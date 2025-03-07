38c476dd403424ab00fd6f8dbbcfe1fd7c3e2035
Iris <Iris.khanatip@gmail.com>
2025-03-08 05:49:35 +0700

    Update

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
 const express = require("express");
 const mysql = require("mysql2");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 // Connect to database using Environment Variables from Railway
 const db = mysql.createPool({
-  uri: process.env.MYSQL_URL || "mysql://root:cSKvpWezJyVavIilvFdEpQalLALHCxud@nozomi.proxy.rlwy.net:34300/railway",
+  uri:
+    process.env.MYSQL_URL ||
+    "mysql://root:cSKvpWezJyVavIilvFdEpQalLALHCxud@nozomi.proxy.rlwy.net:34300/railway",
 });
 
-// Middleware support JSON
+// Middleware to support JSON parsing
 app.use(express.json());
 
-// Testing API
-app.get("/API", (req, res) => {
-  res.send("API is running!");
+// Serve static files from the "frontend" directory
+app.use(express.static(path.join(__dirname, "frontend")));
+
+// Route to serve index.html when accessing the root URL
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "index.html"));
 });
 
-// Start Server
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
