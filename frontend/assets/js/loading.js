9f6dcbf46eed0fcac4363d96a54cdd3f534e4734
Iris <Iris.khanatip@gmail.com>
2025-03-08 07:47:37 +0700

    Update

--- frontend/assets/js/loading.js
+++ frontend/assets/js/loading.js
@@ -1,9 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("Loading screen started...");
-  
-    setTimeout(() => {
-      console.log("Redirecting to home...");
-      window.location.href = "./frontend/pages/home.html";
-    }, 1500); // loading 1.5s
-  });
-  
+  console.log("Loading screen started...");
+
+  setTimeout(() => {
+    console.log("Redirecting to home...");
+    window.location.href = "./pages/home.html";
+  }, 2000); // loading 1.5s
+});
