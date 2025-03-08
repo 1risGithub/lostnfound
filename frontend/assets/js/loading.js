f7cf740fa27758329319ef77737f70d4d3fd819f
Iris <Iris.khanatip@gmail.com>
2025-03-08 07:18:36 +0700

    Update

--- frontend/assets/js/loading.js
+++ frontend/assets/js/loading.js
@@ -1,6 +1,9 @@
 // frontend/js/loading.js
 document.addEventListener("DOMContentLoaded", () => {
+  console.log("Loading screen started...");
+
   setTimeout(() => {
-    window.location.href = "../pages/home.html";
+    console.log("Redirecting to home...");
+    window.location.href = "./pages/home.html";
   }, 1500); // loading 1.5s
 });
