ee8f9524023d1b7a1a26e275a58e8a7e00d616e1
Iris <Iris.khanatip@gmail.com>
2025-03-08 07:58:44 +0700

    Update

--- frontend/assets/js/loading.js
+++ frontend/assets/js/loading.js
@@ -3,6 +3,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
   setTimeout(() => {
     console.log("Redirecting to home...");
-    window.location.href = "./pages/home.html";
+    window.location.href = "./frontend/pages/home.html";
   }, 2000); // loading 1.5s
 });
